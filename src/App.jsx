81c9c6f4292e2411e13b6823dc8a7cb07bc7e7dd
Israel Effum <effumprince345@gmail.com>
2025-07-16 14:40:16 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import gsap from 'gsap';
 
 import { ScrollTrigger } from 'gsap/all';
+import Navbar from './section/Navbar';
+import Hero from './section/Hero';
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
    <main>
-    <div>
-      <h1 className='text-3xl text-pink'>Welcome to the world of GTA</h1>
-    </div>
+   
+  <Navbar></Navbar>
+  <Hero></Hero>
+
    </main>
+   
   )
 }
 
